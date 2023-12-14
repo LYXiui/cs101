2dbad6397a913e6523e0a609c75ac010ddf6bd74
LYXiui <93933547+LYXiui@users.noreply.github.com>
2023-12-14 22:55:20 +0800

    main3.c

---
+++ hw5/main3.c
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for (int y=1,x=1;y<=9 ;x++)
+    {
+        printf("%d*%d=%d\t",y,x,y*x);
+        x<9 ? x:(x=0,y++,printf("\n"));
+    }
+
+    return 0;
+}

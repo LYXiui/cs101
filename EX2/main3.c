4d668689a013d3cd8fcb7eead9c84c17f5f624df
LYXiui <93933547+LYXiui@users.noreply.github.com>
2023-12-14 15:40:29 +0800

    main3.c

---
+++ EX2/main3.c
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int i = 3;
+   if (i % 1){
+       printf("true");
+   }
+   else {
+       printf("false");
+   }
+    return 0;
+}

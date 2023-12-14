82361b6d69cb282e6bd1a0e7f92f1d5b5649b9e0
LYXiui <93933547+LYXiui@users.noreply.github.com>
2023-12-14 22:21:30 +0800

    main2.c

---
+++ hw5/main2.c
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+float leibnizPi(float n) {
+    double pi=1.0;
+    int i;
+    int N;
+    for (i=3, N=2*n+1; i<=N; i+=2)
+        pi += ((i&2) ? -1.0 : 1.0) / i;
+    return 4*pi;
+}
+
+int main(void)
+{
+    printf("Pi: %f\n", leibnizPi(100000000));
+    return 0;
+}

b7a6ff71366a5d543b368a9ad8d8dcf1ad5ce918
LYXiui <93933547+LYXiui@users.noreply.github.com>
2023-12-14 21:11:37 +0800

    main1.c

---
+++ hw5/main1.c
@@ -0,0 +1,23 @@
+using namespace std;
+
+int main()
+{
+    
+    int n = 4;
+    int spc = n;
+    int i, j, k = n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (j>=k)
+            printf("%d ", i);
+            else
+            printf(" ");
+        }
+        k--;
+        printf("\n");
+    }
+
+    return 0;
+}

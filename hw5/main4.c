1a318cda8f53f1bd212342d64f5b1ad99ae60e1d
LYXiui <93933547+LYXiui@users.noreply.github.com>
2023-12-15 23:52:53 +0800

    main4.c

---
+++ hw5/main4.c
@@ -0,0 +1,17 @@
+#include <cstdio>
+
+int N(int num) {
+    int thousands = (num / 1000) % 10;
+    int ones = num % 10;
+
+    return num - (thousands * 1000) + (ones * 1000) + (thousands - ones);
+}
+
+int main() {
+    int num = 1;
+    int swappedNum = N(num);
+
+    printf("對調後數字為：%d\n", swappedNum);
+
+    return 0;
+}

7ae9749223c97d0d30c62a29f8579d27b4237d7c
LYXiui <93933547+LYXiui@users.noreply.github.com>
2023-12-21 11:31:19 +0800

    main3.c

--- EX2/main3.c
+++ EX2/main3.c
@@ -5,10 +5,12 @@ using namespace std;
 int main()
 {
    int i = 3;
-   if (i & 1){
+   if (i & 1)
+   {
        printf("奇數");
    }
-   else {
+   else 
+   {
        printf("偶數");
    }
     return 0;

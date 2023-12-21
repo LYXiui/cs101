3c9567e92c02d550f8b39851ab04aa59afabc1a5
LYXiui <93933547+LYXiui@users.noreply.github.com>
2023-12-21 11:25:36 +0800

    main2.c

--- EX2/main2.c
+++ EX2/main2.c
@@ -5,16 +5,20 @@ using namespace std;
 int main()
 {
    int i = 2;
-   if (i % 15 == 0){
+   if (i % 15 == 0)
+   {
        printf("Love IU");
    }
-   else if (i % 3 == 0){
+   else if (i % 3 == 0)
+   {
        printf("Love");
    }
-   else if (i % 5 == 0){
+   else if (i % 5 == 0)
+   {
        printf("IU");
    }
-   else {
+   else
+   {
        printf("%d/n", i);
    }
     return 0;

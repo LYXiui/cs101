e4488a19f486fcb683f8cad278269e4aee732707
LYXiui <93933547+LYXiui@users.noreply.github.com>
2023-12-21 11:51:41 +0800

    main3.c

--- hw5/main3.c
+++ hw5/main3.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    for (int i = 1; i <= 81; i++) {
+    for (int i = 1; i <= 81; i++) 
+    {
         int y = (i - 1) / 9 + 1;
         int x = i % 9 == 0 ? 9 : i % 9;
 
         printf("%d*%d=%d\t", y, x, y * x);
 
-        if (i % 9 == 0) {
+        if (i % 9 == 0) 
+        {
             printf("\n");
         }
     }
